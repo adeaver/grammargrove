a232ea3fb923d3122e8387561b5bfaf5c5cb79ef
Andrew Deaver <deaver.andrew@gmail.com>
2023-06-09 11:26:06 -0400

    Frontend with docker file

--- frontend/vite.config.development.ts
+++ frontend/vite.config.development.ts
@@ -4,6 +4,11 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  host: true,
+  port: 8000,
+  watch: {
+    usePolling: true,
+  },
   build: {
     rollupOptions: {
         output: {
