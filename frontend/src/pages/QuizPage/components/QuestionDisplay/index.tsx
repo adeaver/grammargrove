4523e1fd6d5b633a1244d971c1a1656fef8ea087
Andrew Deaver <deaver.andrew@gmail.com>
2023-06-30 19:47:09 -0400

    Start display page

--- frontend/src/pages/QuizPage/components/QuestionDisplay/index.tsx
+++ frontend/src/pages/QuizPage/components/QuestionDisplay/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'preact/hooks';
 
-import Text, { TextFunction } from '../../../../components/Text';
+import Text, { TextFunction, TextType } from '../../../../components/Text';
 import LoadingIcon from '../../../../components/LoadingIcon';
 
 import {
@@ -19,39 +19,71 @@ type QuestionDisplayProps = {
 
 const QuestionDisplay = (props: QuestionDisplayProps) => {
     const [ isLoading, setIsLoading ] = useState<boolean>(false);
+    const [ error, setError ] = useState<Error | null>(null);
+
+    const [ correctAnswer, setCorrectAnswer ] = useState<string[] | null>(null);
+    const [ isCorrect, setIsCorrect ] = useState<boolean | null>(null);
+
+    console.log(correctAnswer);
 
     const handleSubmitAnswer = (answer: string[], example_id: string | null | undefined) => {
         setIsLoading(true);
         checkAnswer(
             props.question.id, answer, example_id ? example_id : null,
             (resp: CheckAnswerResponse) => {
-                console.log(resp);
+                setIsLoading(false);
+                setIsCorrect(resp.is_correct);
+                setCorrectAnswer(resp.correct_answer);
             },
             (err: Error) => {
-                console.log(err);
+                setIsLoading(false);
+                setError(err);
             }
         );
     }
 
+    let correctBanner;
+    if (isCorrect) {
+        correctBanner = (
+            <Text type={TextType.Subtitle} function={TextFunction.Confirmation}>
+                Correct!
+            </Text>
+        );
+    } else if (isCorrect != null) {
+        correctBanner = (
+            <Text type={TextType.Subtitle} function={TextFunction.Warning}>
+                Sorry, that isn’t correct!
+            </Text>
+        );
+    }
+
+    let body;
     if (isLoading) {
-        return <LoadingIcon />
+        body = <LoadingIcon />;
+    } else if (!!error) {
+        body = (
+            <Text function={TextFunction.Warning}>
+                Something went wrong, please try again later.
+            </Text>
+        );
     } else if (!!props.question.user_grammar_rule_entry) {
-        return (
+        body = (
             <GrammarRuleQuestionDisplay
                 question={props.question}
                 handleSubmitAnswer={handleSubmitAnswer} />
         );
     } else if (!!props.question.user_vocabulary_entry) {
-        return (
+        body = (
             <VocabularyQuestionDisplay
                 question={props.question}
                 handleSubmitAnswer={handleSubmitAnswer} />
         );
     }
     return (
-        <Text function={TextFunction.Warning}>
-            Something went wrong
-        </Text>
+        <div>
+            { correctBanner }
+            { body }
+        </div>
     );
 }
 
