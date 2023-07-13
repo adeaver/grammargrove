91963bb16acbad37e0c5ad840c20f0fcfd8c8ebf
Andrew Deaver <deaver.andrew@gmail.com>
2023-07-13 15:22:37 -0400

    prep for production uwsgi config

--- grammargrove/settings.py
+++ grammargrove/settings.py
@@ -21,13 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=$5sgkz%%(rbs^##u3=3w7@1$#el(97b2*x%#&q)((n4((7ecw'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("ENV", "prod") == "dev"
-
-ALLOWED_HOSTS = []
-CORS_ORIGIN_ALLOW_ALL = os.environ.get("ENV", "prod") == "dev"
+if os.environ.get("ENV", "prod") == "prod":
+    SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+    DEBUG = False
+    ALLOWED_HOSTS = ["www.grammargrove.com", "grammargrove.com"]
+else:
+    SECRET_KEY = 'django-insecure-=$5sgkz%%(rbs^##u3=3w7@1$#el(97b2*x%#&q)((n4((7ecw'
+    DEBUG = True
+    ALLOWED_HOSTS = []
+    CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
