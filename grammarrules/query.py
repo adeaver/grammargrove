d05f95614c1034d7660318833dd7e97939cf8f9f
Andrew Deaver <deaver.andrew@gmail.com>
2023-08-04 15:02:45 -0400

    Enable experiment

---
+++ grammarrules/query.py
@@ -0,0 +1,23 @@
+from django.db.models import Q, QuerySet
+
+from grammarrules.models import GrammarRuleExample, GrammarRuleExamplePrompt
+
+def get_valid_grammar_rule_examples_for_rule(grammar_rule_id: str, use_only_high_quality: bool = False) -> QuerySet:
+    queryset = get_all_valid_grammar_rules(use_only_high_quality)
+    return queryset.filter(
+        grammar_rule_id=grammar_rule_id
+    )
+
+def get_all_valid_grammar_rules(use_only_high_quality: bool = False) -> QuerySet:
+    examples = GrammarRuleExample.objects.filter(
+        parse_error__isnull=True,
+        grammar_rule_example_prompt__in=GrammarRuleExamplePrompt.objects.filter(
+            is_usable=True,
+        ).values_list("id", flat=True)
+    )
+    if use_only_high_quality:
+        return examples.filter(
+            validation_score__isnull=False,
+            validation_score__gte=400
+        )
+    return examples
